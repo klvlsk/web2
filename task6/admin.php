96b233a0227c52bdf096c8b412fac1428551e2bf
klvlsk <olesyafilka010@gmail.com>
2025-04-21 13:55:08 +0300

    egegeg

--- task6/admin.php
+++ task6/admin.php
@@ -63,7 +63,7 @@ $language_stats = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Админ-панель</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
     <div class="admin-container">
